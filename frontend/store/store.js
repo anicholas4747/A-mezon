f3846b7ae37cf4c217fb8c944394c34350d8ab09
anicholas4747 <an1720@nyu.edu>
2020-03-09 15:35:18 -0400

    logger as dev dependency

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/root_reducer.js';
 import thunk from '../middleware/thunk';
-import {logger} from 'redux-logger';
 
 export const configureStore = () => {
     let preloadedState = {
@@ -15,10 +14,17 @@ export const configureStore = () => {
                 currentUser: window.currentUser
             };
     }
+    const middlewares = [thunk];
+
+    if (process.env.NODE_ENV !== "production") {
+        // must use 'require' (import only allowed at top of file)
+        const { logger } = require("redux-logger");
+        middlewares.push(logger);
+    }
 
     return createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk,logger)
+        applyMiddleware(...middlewares)
     );
 };
